72faeb340dd8fa9ff0d01767bd364014d82dea4d
Andrew Versalle <andrew.versalle@gmail.com>
2020-02-15 14:05:44 -0500

    Created `ParameterNotFoundException` and spec

---
+++ src/Exception/ParameterNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Versalle\Container\Exception;
+
+use Versalle\Container\NotFoundException;
+
+class ParameterNotFoundException extends NotFoundException
+{
+}
